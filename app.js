3d8ee48a2ec49ebd63b120157d0819ce234ea812
Dukenson Dodor <104813218+dodor101@users.noreply.github.com>
2026-01-12 13:46:09 -0600

    Add feature for storing data in local storage

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
-console.log("Task Manager");
+const btnTask = document.querySelector('.btn-task');
+
+class Task {
+  constructor(task, isComplete = false) {
+    this.task = task;
+    this.isComplete = isComplete;
+  }
+}
+
+function createTask(task) {
+  if (task !== '') {
+    const newTask = new Task(task);
+    return newTask;
+  }
+  alert('You Must Enter Task');
+}
+
+const task = createTask('Run 3 miles everyday!');
+
+const addTaskToLocalStorage = (task) => {
+  const existingTasks = JSON.parse(localStorage.getItem('tasks')) || [];
+
+  existingTasks.push(task);
+
+  localStorage.setItem('tasks', JSON.stringify(existingTasks));
+};
+
+btnTask.addEventListener('click', () => {
+  const taskInput = document.querySelector('#task');
+
+  const task = createTask(taskInput.value);
+
+  addTaskToLocalStorage(task);
+});
+
+console.log('duke');
